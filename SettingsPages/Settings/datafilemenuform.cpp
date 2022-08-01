bb9bba7ce7b1a2d36b06f14761d11c4b9359f0b2
kopalatommy <kopalatommy@gmail.com>
2022-08-01 13:32:25 -0600

    Checking that the process to save the current file is being started

--- SettingsPages/Settings/datafilemenuform.cpp
+++ SettingsPages/Settings/datafilemenuform.cpp
@@ -123,6 +123,7 @@ void DataFileMenuForm::OnSaveClicked()
     blockingForm->show();
     if (selectedFile == "Current")
     {
+        qDebug() << "Saving current";
         emit SaveSingleFile(QString(WORKING_DIR).append("datafiles/"), QString("405nm_").append(QDate::currentDate().toString("dd_MM_yy")).append(".csv"), "/media/usb/");
     }
     else if (selectedFile == "All")
